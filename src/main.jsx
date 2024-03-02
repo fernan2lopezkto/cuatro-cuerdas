9ee9ac42adf2cae67de27111771146eebf81acaa
juan <fernan2lopezkto@gmail.com>
2024-03-02 19:15:53 -0300

    coso

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, routes } from 'react-router-dom'
 import tutorials from './data/tutorials.js'
 import TutorialView from './views/TutorialView.jsx'
 
