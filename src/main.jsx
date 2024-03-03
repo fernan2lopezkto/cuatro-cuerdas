04147ad804f74ba132ffe9633fd0c4ae43af06d2
juan <fernan2lopezkto@gmail.com>
2024-03-02 22:40:34 -0300

    ggg

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import About from './pages/About.jsx';
+import ErrorPage from './error-page';
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/cuatro-cuerdas",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/cuatro-cuerdas/about",
+    element: <About />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
