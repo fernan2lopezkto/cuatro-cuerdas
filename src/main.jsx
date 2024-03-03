b47bcabe93ff93c12279b902c309c065bf9207e3
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-03-03 13:20:40 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,12 +12,28 @@ import {
 const router = createBrowserRouter([
   {
     path: "/cuatro-cuerdas",
-    element: <App />,
+    element: <div><h1>Inicio</h1></div>,
     errorElement: <ErrorPage />
   },
   {
     path: "/cuatro-cuerdas/about",
     element: <About />,
+    errorElement: <ErrorPage />
+  },
+    {
+    path: "/cuatro-cuerdas/contact",
+    element: <div><h1>Contacto</h1></div>,
+    errorElement: <ErrorPage />
+  },
+    {
+    path: "/cuatro-cuerdas/tutoriales",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+    {
+    path: "/cuatro-cuerdas/*",
+    element: <div><h2>Pagina no encontrada</h2></div>,
+    errorElement: <ErrorPage />
   },
 ]);
 
