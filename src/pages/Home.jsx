fe6fdc0dd3ebb8a5e9dc588b03faca870ad41fd9
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-03-04 23:30:04 -0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1>Inicio</h1>
+      <Footer />
+    </>
+  );
+}
