61cf13077a693659c93b5ccaf4397731132dcc61
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-03-13 13:18:28 -0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import "./Card.css"
 
-function Card({ title = "titulo por defecto", description = "descripcion por defecto" }) {
+function Card({ path = "titulo por defecto", description = "descripcion por defecto" }) {
   return (
     <div className="Card">
       <h2>
-        <Link to={"/cuatro-cuerdas/tutoriales" +title}>
-        {title}
+        <Link to={"/cuatro-cuerdas/tutoriales" + path}>
+        {path}
         </Link>
       </h2>
       <p>{description}</p>
