dedd201a765a35f63ea1c17e745c7dbf74f8ddd5
juan <fernan2lopezkto@gmail.com>
2024-03-13 11:41:59 -0300

    ddd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import "./Home.css"
+import VideoPlayer from "../components/VideoPlayer";
+import "./Home.css";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <div className="page-container">
-      <div className="home-titulo-container">
-        <h1>Cuatro Cuerdas</h1>
-        <p>Tutoriales</p>
-      </div>
-        <div className="home-video-container">aqui va el vídeo </div>
+        <div className="home-titulo-container">
+          <h1>Cuatro Cuerdas</h1>
+          <p>Tutoriales</p>
+        </div>
+        <div className="home-video-container">
+          <div>
+            <VideoPlayer url="https://www.youtube.com/watch?v=zuFbI7I_-RA" />
+          </div>
+        </div>
       </div>
       <Footer />
     </>
