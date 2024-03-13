87fdd855ce0061861ac8d25d918d1f1a044221c9
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-03-13 12:58:20 -0300

    Update VideoPlayer.jsx

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import ReactPlayer from "react-player/youtube";
 
 function VideoPlayer({ url }) {
   return (
-    <div className="vidPlay">
+    <div style={{ width: '100%', height: '100%' }}>
       <ReactPlayer url={url} controls={true} volume={0.5} />
     </div>
   );
