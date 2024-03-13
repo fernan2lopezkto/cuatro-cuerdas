6876ed1ab82ca02bb465080ec31955d08a112637
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-03-13 18:40:21 -0300

    Update tutorials.js

--- src/data/tutorials.js
+++ src/data/tutorials.js
@@ -4,7 +4,7 @@ const tutorials = [
       path: "ven-espiritu-santo-pista-guitarra-bajo-piano",
       title: "Ven Espiritu Santo",
       description: "Ven Espíritu Santo Barak con acordes, guitarra, piano, bajo y pistas para ensayar",
-      videoURL: "https://youtu.be/gV7RkJtBOjU?si=by7IH6QppjSzZrIp",
+      videoURL: "https://youtu.be/gV7RkJtBOjU?si=zXjqhDBQ7FLIKT59",
       lyrics: "INTRO:      MIm      RE     DO     SI 7    8 TIEMPOS CADA NOTA\nMIm\n                                     RE\n      Estoy aquí, desesperado por ti\n                      DO                                             SI 7\nCon un corazón sediento, que espera beber de ti\nDOCuando tu gloria desciende a un lugar\nRE\nToda la tierra tiene que adorar\n            LAm\nResucitan los muertos se sanan enfermos         SI\npor tu poder.\nDO\Queremos de ti llénanos de ti\nRE\nEspíritu santo envuélvenos en ti\n       LAm                                SI 7\nDerrama tu gloria, Esperamos por ti\n",
       image:
         "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F0x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349",
