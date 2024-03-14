8bd0653a0608bbb08fa3bd107cd7e917a1ea1397
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-03-14 07:36:07 -0300

    Update TutorialPage.jsx

--- src/pages/TutorialPage.jsx
+++ src/pages/TutorialPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { useEffect, useState } from 'react';
 import "./TutorialPage.css";
 import Navbar from "../components/Navbar";
 import Foot from "../components/Footer";
 import VideoPlayer from "../components/VideoPlayer";
 
 function TutorialPage({ tutorial }) {
+
+  function LyricsComponent({ tutorial }) {
+  const [lyrics, setLyrics] = useState('');
+
+  useEffect(() => {
+    async function fetchLyrics() {
+      try {
+        // Importar el contenido del archivo de texto
+        const { default: textContent } = await import(tutorial.path);
+        setLyrics(textContent);
+      } catch (error) {
+        console.error('Error al cargar el archivo:', error);
+      }
+    }
+
+    fetchLyrics();
+  }, [tutorial]);
+    
   return (
     <div>
       <Navbar />
@@ -16,7 +35,7 @@ function TutorialPage({ tutorial }) {
             <VideoPlayer url={tutorial.videoURL} />
         </div>
           <div>
-              <pre>{tutorial.lyric}</pre>
+              <pre>{lyric}</pre>
           </div>
           <div>{tutorial.longDescription}</div>
       <Foot />
